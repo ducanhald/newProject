c9f46a5bfae719eb61751b002c2a9701e073bb99
ducanh <ducanhald.@gmail.com>
2021-05-14 18:45:15 +0700

    static file, scss

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 const path = require('path')
 const express = require('express')
 const handlebars = require('express-handlebars')
+const { static } = require('express')
 const app = express()
 const port = 3000
 
-app.engine('hbs', handlebars({
-        extname: '.hbs'
-    }
+app.use(express.static(path.join(__dirname, 'public')))
 
-))
+app.engine('hbs', handlebars({
+    extname: '.hbs'
+}))
+
+
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'resource/views'))
 
