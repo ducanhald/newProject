f701f1970f367535e4306a47b45ca41b7c6680f9
ducanh <ducanhald.@gmail.com>
2021-06-17 22:24:48 +0700

    Prettier - Code formatter

--- src/index.js
+++ src/index.js
@@ -1,28 +1,33 @@
-const path = require('path')
-const express = require('express')
-const handlebars = require('express-handlebars')
-const { static } = require('express')
-const app = express()
-const port = 3000
+const path = require('path');
+const express = require('express');
+const handlebars = require('express-handlebars');
+const { static } = require('express');
+const app = express();
+const port = 3000;
 
-const route = require('./routes')
+const route = require('./routes');
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.engine('hbs', handlebars({
-    extname: '.hbs'
-}))
+app.engine(
+  'hbs',
+  handlebars({
+    extname: '.hbs',
+  }),
+);
 
-app.use(express.urlencoded({
-    extended: true
-}))
-app.use(express.json())
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+);
+app.use(express.json());
 
-app.set('view engine', 'hbs')
-app.set('views', path.join(__dirname, 'resource/views'))
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resource/views'));
 
 route(app);
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
