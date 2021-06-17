913958257c86d3176436eca3d1a2d8c4a5dd2efc
ducanh <ducanhald.@gmail.com>
2021-06-17 22:33:29 +0700

    a

--- src/routes/news.js
+++ src/routes/news.js
@@ -3,9 +3,9 @@ const router = express.Router();
 
                 const newsController = require('../app/controllers/NewsController');
 
-newsController.index;
+    newsController.index;
 
                 router.use('/:slug', newsController.show);
-router.use('/', newsController.index);
+    router.use('/', newsController.index);
 
 module.exports = router;
